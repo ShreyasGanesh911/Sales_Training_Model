3244e59e4373e50434f08577776a896f3e03ec03
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-05-06 22:54:16 +0530

    added gpt router

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,13 @@ import express, { NextFunction, Request, Response } from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import APIError from "./utils/APIError.js"
+import gptRoute from "./routes/gpt.route.js"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
-
+app.use("/api/v1/gpt",gptRoute)
 
 
 app.use(APIError)
