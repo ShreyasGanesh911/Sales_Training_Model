ded450eafa9a02c4370b3c89560694d78b2d0e96
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-05-07 18:20:11 +0530

    added message bubble

--- client/src/Pages/Chat.tsx
+++ client/src/Pages/Chat.tsx
@@ -1,28 +1,36 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Input from "../Components/Input";
-import MicButton from "../Components/MicButton";
-
+import MessageBubble from "../Components/MessageBubble";
+interface Message {
+  text: string;
+  isUser: boolean;
+  timestamp: Date;
+  type: 'text' | 'video' | 'audio'
+  url?: string
+}
 const Chat = () => {
     const messagesEndRef = useRef<HTMLDivElement>(null);
+    const chatContainerRef = useRef<HTMLDivElement>(null);
+    const [messages, setMessages] = useState<Message[]>([
+      {
+        text: "Demo Message",
+        isUser: false,
+        timestamp: new Date(),
+        type: 'text'
+      }
+    ]);
   return (
     <>
        <div className="flex justify-center items-start min-h-screen bg-gray-50 p-4">
        <div className="w-full 2xl:max-w-[40%] xl:max-w-[70%] relative h-[95vh] bg-white rounded-2xl shadow-lg">
-        {/* <div ref={chatContainerRef} className="h-full overflow-y-auto pb-40 px-6 pt-6">
+        <div ref={chatContainerRef} className="h-full overflow-y-auto pb-40 px-6 pt-6">
           {messages.map((message, index) => (
-            <MessageBubble  key={message.id || index} index={index}  message={message}
-              currentText={message.id === currentMessageId ? currentText : message.text}/>
+            <MessageBubble  key={index} index={index}  message={message}/>
           ))}
-        //   <div ref={messagesEndRef} />
-        </div> */}
+           <div ref={messagesEndRef} />
+        </div>
         <div className="absolute bottom-0.5 w-full left-0  rounded-2xl right-0 px-6 bg-white mr-5">
-            {/* <SessionControls   startSession={startSession} stopSession={stopSession}
-              sendClientEvent={sendClientEvent} sendTextMessage={sendTextMessage}
-              serverEvents={events} isSessionActive={isSessionActive} />
-            {isSessionActive && <MicButton disableMicrophone={disableMicrophone} enableMicrophone={enableMicrophone} isMicActive={isMicActive}/> } */}
-            <Input/>
-            {/* <MicButton isMicActive={false} enableMicrophone={() => {}} disableMicrophone={() => {}}/> */}
-            
+            <Input setMessages={setMessages}/>
         </div>
         </div>
        </div>
