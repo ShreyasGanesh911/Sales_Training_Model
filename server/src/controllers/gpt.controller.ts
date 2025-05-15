83cbb8584200de45726901f4aee87546a363604f
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-05-15 18:12:58 +0530

    added message parsing from request body

--- server/src/controllers/gpt.controller.ts
+++ server/src/controllers/gpt.controller.ts
@@ -5,9 +5,15 @@ import getWhisperTranscript from "../utils/WhisperTranscript";
 import uploadToCloudinary from "../utils/UploadToCloudinary";
 import gptText from "../utils/GPTText";
 import { sales_script } from "../script/script";
+enum Role {
+    USER = "user",
+    ASSISTANT = "assistant",
+    SYSTEM = "system"
+}
 export const handleText = AsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
     const {text,messages} = req.body;
-    const response = await gptText(text,messages);
+   
+    const response = await gptText(text,messages,Role.USER);
     if(!response){
         next(new ErrorHandler("Failed to generate a response from GPT",500))
         return 
@@ -21,6 +27,9 @@ export const handleText = AsyncHandler(async(req:Request,res:Response,next:NextF
 
 export const handleAudio = AsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
     const file = req.file as Express.Multer.File;
+    const messages = JSON.parse(req.body.messages);
+    // console.log(messages)
+    
     // console.log(file)
     if(!file){
         return next(new ErrorHandler("No audio file provided",400));
@@ -28,15 +37,18 @@ export const handleAudio = AsyncHandler(async(req:Request,res:Response,next:Next
     try {
         const transcript = await getWhisperTranscript(file.path);
         const audioUrl = await uploadToCloudinary(file.path);
+        const gptResponse = await gptText(transcript,messages,Role.USER);
         res.status(200).json({
             success: true,
             message: "Audio transcribed successfully",
             data: {
                 transcript,
-                url:audioUrl?.url
+                url:audioUrl?.url,
+                gptResponse
             }
         });
     } catch (error) {
+        console.log(error)
         return next(new ErrorHandler("Failed to transcribe audio" + error,500))
     }
 })
@@ -44,27 +56,30 @@ export const handleAudio = AsyncHandler(async(req:Request,res:Response,next:Next
 export const handleVideo= AsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
     const file = req.file as Express.Multer.File;
     // console.log(file)
+    const messages = JSON.parse(req.body.messages);
     if(!file){
         return next(new ErrorHandler("No video file provided",400));
     }
     const transcript = await getWhisperTranscript(file.path);
     const videoUrl = await uploadToCloudinary(file.path);
+    const gptResponse = await gptText(transcript,messages,Role.USER);
     const videoUrlString = videoUrl?.url;
 
     // console.log(videoUrl)
     res.status(200).json({
         success:true,
-        message:"Audio transcript",
+        message:"Video transcript",
         data:{
             transcript,
-            url:videoUrlString
+            url:videoUrlString,
+            gptResponse
         }
     })
 })
 
 export const handleAssessmentStart = AsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
     const text = sales_script;
-    const response = await gptText(text,[]);
+    const response = await gptText(text,[],Role.SYSTEM);
     res.status(200).json({
         success:true,
         message:"Assessment started",
