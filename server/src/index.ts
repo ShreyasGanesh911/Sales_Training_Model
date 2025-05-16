9d7e3ab25af134a412fa3123054eba4b2dd72257
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-05-16 18:53:55 +0530

    added static file rendering

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import app from "./app.js";
 import "dotenv/config"
-const port = process.env.PORT || 8000
-app.get("/",(req,res)=>{
-    res.send("Hello World")
+import { Request,Response } from "express";
+import path from "path"
+import express from "express";
+const port = process.env.PORT || 8888
+
+// Serve static files from the React dist directory
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+// Handle all other routes by serving index.html
+app.get("*",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"))
 })
+
 app.listen(port,()=>{
     console.log("Active at port",port)
 })
