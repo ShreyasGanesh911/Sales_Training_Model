502c5db2d59f755c97e3aba3c8b7d6b7f2ec84f3
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-06-06 19:07:34 +0530

    improved error handling

--- server/src/utils/WhisperTranscript.ts
+++ server/src/utils/WhisperTranscript.ts
@@ -12,6 +12,7 @@ const getWhisperTranscript = async (audioFilePath: string): Promise<string> => {
     const formData = new FormData();
     formData.append("file", fs.createReadStream(audioFilePath));
     formData.append("model", "whisper-1");
+    formData.append("language", "en");
     const respose:ResponseObject = await axios.post("https://api.openai.com/v1/audio/transcriptions",formData,{
       headers:{
         "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
@@ -19,12 +20,16 @@ const getWhisperTranscript = async (audioFilePath: string): Promise<string> => {
       },
     })
     const transcript = respose.data.text
-    console.log(transcript)
+    // console.log(transcript)
+    if(transcript.length<1){
+      console.log("No text detected in the audio")
+      throw new ErrorHandler("No text detected in the audio", 400);
+    }
     return transcript
 
   } catch (error) {
     console.error("Error getting transcript:", error);
-    throw new ErrorHandler("Failed to get transcript" + error, 500);
+    throw new ErrorHandler("Failed to get transcript: No audio detected", 500);
   }
 };
 
