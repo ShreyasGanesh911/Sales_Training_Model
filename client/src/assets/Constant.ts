3874c35570c14b3434a2e43486153ae2f57cd554
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-06-17 16:51:10 +0530

    Added uploading texts

---
+++ client/src/assets/Constant.ts
@@ -0,0 +1,11 @@
+export const welcomeMessage = `**Welcome to AI Sales Training!** 👋 \n 
+Get ready to practice and perfect your sales skills with your personal AI Sales Coach. 
+This interactive training will guide you through realistic customer scenarios, helping you master key sales techniques, build confidence, and improve your responses with instant feedback. To begin your journey click the **Start Assessment** button below.`
+
+export const uploadMessages = [
+    'Cooking up your content… almost there 🍿',
+    'Uploading your cinematic genius…',
+    'Adding the final touches to your masterpiece 🎬',
+    'Rolling out the red carpet for your video…',
+    'Hang tight, your blockbuster is on its way! 🎥'
+  ];
